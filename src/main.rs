f7c71e1bb1b01efe83ebe6fd38af402a954d42c3
Casey Donald Peat <cpe44@cs17062bq.canterbury.ac.nz>
2020-10-22 22:01:31 +1300

    this might take a while...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+struct Array {
+    dims: i64,
+    array: i64,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let array:[[f32; 4]; 4]
+    println!("{}", array)
 }
